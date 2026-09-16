3cce7f469d38f8295bc7c2cc1822e420edfd90c0
DangTanPhat <phat.2474802010292@vanlanguni.vn>
2026-09-16 17:02:45 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,31 +1,8 @@
 function login(username, password) {
-  // Kiểm tra username rỗng
-  if (!username) {
-    throw new Error("Username không đựợc để trống");
-  }
-
-  // Kiểm tra password rỗng
-  if (!password) {
-    throw new Error("Password không được để trống");
-  }
-
-  // Tài khoản bị khóa
-  if (username === "locked") {
-    throw new Error("Tài khoản đã bị khóa");
-  }
-
-  // Mật khẩu chứa ký tự đặc biệt
-  if (!/^[a-zA-Z0-9]+$/.test(password)) {
-    throw new Error("Mật khẩu không được chứa ký tự đặc biệt");
-  }
-
-  // Kiểm tra tài khoản và mật khẩu
   if (username === "admin" && password === "123") {
     return true;
   }
-
-  // Mật khẩu sai
-  throw new Error("Sai username hoặc password");
+  return false;
 }
 
 module.exports = { login };
